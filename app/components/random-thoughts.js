7bc8df2383205d91729ea8dd2ab3d318d21e78dd
Gordon Potter <eccegordo@gmail.com>
2016-04-02 12:07:58 -0700

    add button to change thoughts

--- app/components/random-thoughts.js
+++ app/components/random-thoughts.js
@@ -2,4 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   thinkyBrain: Ember.inject.service('thinky-brain'),
+  thoughts: null,
+  init(){
+    this._super();
+    this.set('thoughts', this.get('thinkyBrain').get('thoughts'));
+  },
+
+  actions: {
+    changeThoughts: function(){
+      let thoughts = this.get('thinkyBrain').get('thoughts');
+      this.set('thoughts', thoughts);
+    }
+  }
 });
